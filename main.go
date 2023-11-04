dbc4c6d2c3a1376c16412e12705ee094f33f4b6b
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2023-11-04 19:04:19 +0700

    feat: add post, get all, and get id

---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Customer struct {
+	Name      string `json:"name"`
+	ID        string `json:"id"`
+	Role      string `json:"role"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Contacted bool   `json:"contacted"`
+}
+
+var customers []Customer = []Customer{
+	{
+		Name:      "John Doe",
+		ID:        "001",
+		Role:      "customer",
+		Email:     "johndoe@gmail.com",
+		Phone:     "1234567890",
+		Contacted: false,
+	},
+	{
+		Name:      "Jane Doe",
+		ID:        "002",
+		Role:      "customer",
+		Email:     "janedoe@gmail.com",
+		Phone:     "9876543210",
+		Contacted: false,
+	},
+	{
+		Name:      "John Smith",
+		ID:        "003",
+		Role:      "customer",
+		Email:     "johnsmith@gmail.com",
+		Phone:     "1234567890",
+		Contacted: false,
+	},
+}
+
+// The function should return a brief overview of the API (e.g., available endpoints).
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html, charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	// serve html file
+	http.ServeFile(w, r, "index.html")
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(customers)
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, customer := range customers {
+		if customer.ID == params["id"] {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(customer)
+			return
+		}
+	}
+
+	// return 404 if customer not found
+	w.WriteHeader(http.StatusNotFound)
+	// return json message if customer not found
+	json.NewEncoder(w).Encode(struct {
+		Message string `json:"message"`
+	}{
+		Message: "Customer not found",
+	})
+}
+
+func addCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var customer Customer
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &customer)
+
+	// check if customer already exists
+	for _, c := range customers {
+		if c.ID == customer.ID {
+			w.WriteHeader(http.StatusConflict)
+			json.NewEncoder(w).Encode(struct {
+				Message string `json:"message"`
+			}{
+				Message: "Customer already exists",
+			})
+			return
+		}
+	}
+
+	customers = append(customers, customer)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(customer)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", index).Methods("GET")
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
+
+	println("Server is listening on port 3000...")
+	log.Fatal(http.ListenAndServe(":3000", router))
+
+	// check if port is in use
+	// l, err := net.Listen("tcp", ":3000")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer l.Close()
+	// log.Println("Server is listening on port 3000...")
+	// http.ListenAndServe(":3000", router)
+}
